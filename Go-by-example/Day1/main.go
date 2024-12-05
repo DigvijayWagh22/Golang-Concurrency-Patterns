95c8a361b73013041bfcd7a2c5e4bdf36882d4b4
DigvijayWagh22 <waghdigvijay2010@gmail.com>
2024-12-05 14:50:54 +0530

    for, if - else

--- Go-by-example/Day1/main.go
+++ Go-by-example/Day1/main.go
@@ -51,4 +51,58 @@ func main() {
 	fmt.Println(int64(n1))
 	fmt.Println(math.Sin(n))
 
+	// for loop
+
+	i := 1
+	for i <= 3 { // basic for loop external value declaration
+		fmt.Println(i)
+		i++
+	}
+	fmt.Println()
+
+	for j := 0; j < 3; j++ { // basic for loop with internal declaration
+		fmt.Println(j)
+	}
+	fmt.Println()
+
+	for i := range 3 { // iterates the range number times
+		fmt.Println("range ", i)
+	}
+
+	for { // break condition
+		fmt.Println("will print once and then break")
+		break
+	}
+
+	for n := range 6 { // continue condition
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+
+	// If/Else
+	if 7%2 == 0 { // basic if /else
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
+
+	if 8%4 == 0 { // basic if
+		fmt.Println("8 is divisible by 4")
+	}
+
+	if 8%2 == 0 || 7%2 == 0 { // logical condition based condition
+		fmt.Println("Either 7 or 8 are even")
+	}
+
+	if num := 9; num < 0 { // If - Else if - else ladder
+		fmt.Println("num is negative")
+	} else if num < 10 {
+		fmt.Println("num has 1 digit")
+	} else {
+		fmt.Println("num has multiple digtis")
+	}
+	// switch statement
+
 }
