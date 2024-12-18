b2a6e9378714c0f509d747c6196122a1c79597d4
DigvijayWagh22 <waghdigvijay2010@gmail.com>
2024-12-18 10:42:09 +0530

    Enums added

--- Go-by-example/Day6/main.go
+++ Go-by-example/Day6/main.go
@@ -5,6 +5,8 @@ import (
 	"math"
 )
 
+//Interfaces
+
 type geometry interface {
 	area() float64
 	peri() float64
@@ -37,9 +39,54 @@ func measure(g geometry) {
 	fmt.Println("perimeter ", g.peri())
 }
 
+//Enums
+
+type ServerState int
+
+const (
+	StateIdle ServerState = iota
+	StateConnected
+	StateError
+	StateRetrying
+)
+
+var stateName = map[ServerState]string{
+	StateIdle:      "idle",
+	StateConnected: "connected",
+	StateError:     "Error",
+	StateRetrying:  "retrying",
+}
+
+func (ss ServerState) String() string {
+	return stateName[ss]
+}
+
+func transition(s ServerState) ServerState {
+	switch s {
+	case StateIdle:
+		return StateConnected
+	case StateConnected, StateRetrying:
+		return StateIdle
+	case StateError:
+		return StateError
+	default:
+		panic(fmt.Errorf("Unknown state: %s", s))
+	}
+}
+
 func main() {
+	// Interfaces
 	r := rect{5, 10}
 	c := circle{10}
 	measure(r)
 	measure(c)
+
+	//Enums
+
+	ns := transition(StateIdle)
+	fmt.Println(ns)
+
+	ns2 := transition(ns)
+	fmt.Println(ns2)
+
 }
